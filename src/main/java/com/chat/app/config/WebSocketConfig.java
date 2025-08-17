e36a1f02ff0ffef8b433ee00a66fe6f3846edf6f
Kanav Rohatgi <kanavr@gmail.com>
2025-08-18 02:03:01 +0530

    Configure WebSocket

---
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -0,0 +1,33 @@
+package com.chat.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer 
+{
+
+    // Subscribe (SUB)
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("http://localhost:3000")
+                .withSockJS();
+    }
+
+    // Publish (PUB)
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");  // routes the message to subscribed users
+        registry.setApplicationDestinationPrefixes("/app"); // in for client
+    }
+    
+    // PUB/SUB concept
+    
+
+
+}
